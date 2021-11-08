cc122816c71f50a48de5b3c554df7d93b643ba8e
yugmade13 <yugmade@gmail.com>
2021-11-08 14:51:48 +0700

    menambhakan cache API

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -6,6 +6,7 @@ const CONFIG = {
     KEY: '12345',
     BASE_URL: 'https://restaurant-api.dicoding.dev/',
     DEFAULT_LANGUAGE: 'en-us',
+    CACHE_NAME: 'RestaurantCatalogue-V1',
 };
 
 export default CONFIG;
